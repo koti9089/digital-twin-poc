536dbb9b888ed2f27a41d67dee82745fa7f2beaa
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-02-17 13:58:57 +0530

    adds swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('digital-twin-poc')
+    .setDescription('Api for Buildings Crud (gremlin-api)')
+    .setVersion('1.0')
+    .addTag('Building')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
   await app.listen(8080);
 }
 bootstrap();
