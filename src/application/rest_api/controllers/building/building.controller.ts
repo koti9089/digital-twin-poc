35b15a1fba9e4164669d5398235b19744dd82e3a
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-02-18 15:55:26 +0530

    adds methods

--- src/application/rest_api/controllers/building/building.controller.ts
+++ src/application/rest_api/controllers/building/building.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Building } from 'src/domain/buildings/building/building';
 import { BuildingRepository } from 'src/infrastructure/data_access/gremlin/repositories/building.repository';
+import { Serialize } from 'src/infrastructure/interceptors/serialize.interceptor';
+import { BuildingDto } from './dtos/building.dto';
+import { CreateBuildingDto } from './dtos/create-building.dto';
+import { AllBuildingDto } from './dtos/get-all-building.dto';
 
 @ApiTags('Building')
 @Controller('building')
 export class BuildingController {
   constructor(private buidingRepo: BuildingRepository) {}
+
+  @Serialize(AllBuildingDto)
   @Get('/all')
-  async getAllBuildings(): Promise<string> {
-    return 'Gets all buildings';
+  async getAllBuildings() {
+    console.log('Running');
+    const buildings = await this.buidingRepo.getAllBuildings();
+    console.log(
+      '🚀 ~ file: building.controller.ts:20 ~ BuildingController ~ getAllBuildings ~ buildings',
+      buildings,
+    );
+    return buildings;
+  }
+
+  @Serialize(BuildingDto)
+  @Post()
+  async createBuilding(@Body() body: CreateBuildingDto) {
+    const building = Building.create(body);
+    const result = await this.buidingRepo.createBuilding(building);
+    return result;
+  }
+
+  @Serialize(BuildingDto)
+  @Get('/:id')
+  async getBuilding(@Param('id') id: string) {
+    return this.buidingRepo.getBuilding(id);
+  }
+
+  @Delete('/:id')
+  async deleteBuilding(@Param('id') id: string) {
+    return this.buidingRepo.deleteBuilding(id);
   }
 }
