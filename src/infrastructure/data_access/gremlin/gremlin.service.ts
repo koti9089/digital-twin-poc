4d1c75be74520cc459b2ec3106e9938469568283
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-02-18 11:00:44 +0530

    remove temp methods and makes _client public

--- src/infrastructure/data_access/gremlin/gremlin.service.ts
+++ src/infrastructure/data_access/gremlin/gremlin.service.ts
@@ -8,7 +8,7 @@ export class GremlinService {
   private readonly _primaryKey: string;
   private readonly _database: string;
   private readonly _collection: string;
-  private readonly _client;
+  public readonly _client;
   constructor(private configService: ConfigService) {
     this._endpoint = configService.get<string>('COSMOS_GREMLIN_ENDPOINT');
     this._primaryKey = configService.get<string>('COSMOS_GREMLIN_PRIMARY_KEY');
@@ -23,30 +23,5 @@ export class GremlinService {
       rejectUnauthorized: true,
       mimeType: 'application/vnd.gremlin-v2.0+json',
     });
-  }
-
-  async dropGraph() {
-    console.log('Running Drop');
-    return this._client.submit('g.V().drop()', {}).then(function (result) {
-      console.log('Result: %s\n', JSON.stringify(result));
-    });
-  }
-
-  async addVertex1() {
-    console.log('Running Add Vertex1');
-    return this._client
-      .submit(
-        "g.addV(label).property('id', id).property('firstName', firstName).property('age', age).property('userid', userid).property('pk', 'pk')",
-        {
-          label: 'person',
-          id: 'thomas',
-          firstName: 'Thomas',
-          age: 44,
-          userid: 1,
-        },
-      )
-      .then(function (result) {
-        console.log('Result: %s\n', JSON.stringify(result));
-      });
   }
 }
