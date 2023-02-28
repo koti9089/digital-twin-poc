79f9954321b5a505a8099c6776fa16ce94d83164
Mohammad Faisal Kuchay <Mohammad.Faisal@neudesic.com>
2023-02-28 11:58:04 +0530

    removes tag

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     .setTitle('digital-twin-poc')
     .setDescription('Api for Buildings Crud (gremlin-api)')
     .setVersion('1.0')
-    .addTag('Airport')
+    .addTag('Country')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
